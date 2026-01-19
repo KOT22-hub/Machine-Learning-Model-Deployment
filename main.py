e8940cd773944011edd74de5a50a8653a2ef5871
KOT22-hub <munatsi272@gmail.com>
2026-01-19 14:29:24 +0200

    added python code

--- main.py
+++ main.py
@@ -29,3 +29,38 @@ cases = pd.DataFrame({
 clients.head(30)
 staff.head(30)
 cases.head(20)
+df = cases.merge(clients,on="client_id").merge(staff,on="staff_id")
+X= df[['age', 'risk_score', 'years_exp', 'workload', 'priority']]
+y= df[["is_resolved"]]
+
+df.head()
+# 20% of the data to "test" the model's accuracy
+X_train,X_test,Y_train,Y_test = train_test_split(X,y,test_size=0.2, random_state=42)
+# train the model 
+tree_model = DecisionTreeClassifier(max_depth =3,random_state=42 )
+tree_model.fit(X_train,Y_train)
+y_pred = tree_model.predict(X_test)
+print("Model Performance ")
+print(classification_report(Y_test, y_pred))
+from skl2onnx import convert_sklearn
+from skl2onnx.common.data_types import FloatTensorType
+
+# 1. Defined the input shape (5 features)
+initial_type = [('float_input', FloatTensorType([None, 5]))]
+
+# 2. Disable 'zipmap'. This forces probabilities to be a standard 
+# Tensor (array) instead of a complex Map object.
+options = {type(tree_model): {'zipmap': False}}
+
+# 3. Convert with the new options
+onx = convert_sklearn(
+    tree_model, 
+    initial_types=initial_type, 
+    options=options
+)
+
+# 4. Saved the new file 
+with open("case_predictor.onnx", "wb") as f:
+    f.write(onx.SerializeToString())
+
+print("✅ Model re-exported with ZipMap disabled!")
